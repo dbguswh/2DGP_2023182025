4ee17931e6524b7faecfb711dc1d43b4f7cc58eb
YOU HYUN JO <guswh0820@tukorea.ac.kr>
2026-09-23 01:40:47 +0900

    세번째 변의 범위 설정과 캐릭터 이동 처리

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -43,6 +43,14 @@ def move_rectangle():
         update_canvas()
         y += 2
         delay(0.01)
+
+    x = 600
+    while (x >= 200):
+        clear_canvas()
+        character.draw(x, 450)
+        update_canvas()
+        x -= 2
+        delay(0.01)
     pass
 
 def move_triangle():
